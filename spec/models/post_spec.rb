e78ec4ff22a2322d592a983404595b60e3ea5199
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-10 19:56:30 -0500

    rspec for post validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,7 +6,10 @@ describe Post do
     it { should have_many :comments}
   end
 
-
+  describe 'validation' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
   
 end
 
