33bed07a2bfaaa95f376f9f1d2accc881fabe7ed
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-10 19:42:35 -0500

    rspec for all the associations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Post do
   describe 'association' do
     it { should belong_to :user }
+    it { should have_many :comments}
   end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe User do
   describe 'association' do
     it { should have_many :posts}
+    it { should have_many :comments}
   end
 end
