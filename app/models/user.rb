3fc2a23d4f8a2557aabf3d7ea9ce017e7a38474f
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-10 19:24:43 -0500

    rspec for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :trackable, :validatable
+
+
+  has_many :posts
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-
+  describe 'association' do
+    it { should have_many :posts}
+  end
 end
