4352910b1c6cf323aa594fbc9140e5979e1806b7
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-13 16:36:39 -0500

    some change

--- db/migrate/20140213205909_add_comment_count_to_posts.rb
+++ db/migrate/20140213205909_add_comment_count_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCommentCountToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :comment_count, :integer
+    add_column :posts, :comment_count, :integer, default: 0
   end
 end
